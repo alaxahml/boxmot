4c6e7f05aae6020a87d1e2f51e919086782bd87a
alaxahml <45281963+alaxahml@users.noreply.github.com>
2025-07-17 18:18:39 +0300

    Update botsort.py

--- boxmot/trackers/botsort/botsort.py
+++ boxmot/trackers/botsort/botsort.py
@@ -319,6 +319,7 @@ class BotSort(BaseTracker):
                 #print("AFTER EMBEDDING_DIST", equal_matrix)
                 equal_matrix[equal_matrix == True] = 0.25
                 equal_matrix[equal_matrix == False] = 0.5
+                print("AFTER LOGIC", equal_matrix)
                 #emb_dists[emb_dists > appearance_thresh] = 1.0
                 
                 ious_dists_mask = ious_dists > proximity_thresh
