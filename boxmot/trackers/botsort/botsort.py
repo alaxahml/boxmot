051ceec4aeed23ac1f4a54335905ea2085812161
alaxahml <45281963+alaxahml@users.noreply.github.com>
2025-07-28 15:45:51 +0300

    Update botsort.py

--- boxmot/trackers/botsort/botsort.py
+++ boxmot/trackers/botsort/botsort.py
@@ -120,37 +120,37 @@ class BotSort(BaseTracker):
 
 
 
-            
-        matches_lost, u_track_lost, u_det_lost = self._lost_association(
-            activated_stracks,
-            refind_stracks,
-            detections,
-            active_tracks,
-            unconfirmed,
-            img,
-            dets,
-        )
-
-        
-
-
-        #print("MATCHES ACTIVE:", matches_active)
-
-        remaining_dets = [detections[i] for i in u_det_lost]
-
         # First association
         matches_active, u_track_active, u_det_active = self._first_association(
             dets,
             active_tracks,
             unconfirmed, 
             img,
-            remaining_dets,
+            detections,
             activated_stracks,
             refind_stracks,
         )
+    
 
 
-        remaining_dets = [remaining_dets[i] for i in u_det_active]
+        
+
+
+        #print("MATCHES ACTIVE:", matches_active)
+
+        remaining_dets = [detections[i] for i in u_det_active]
+
+
+        matches_lost, u_track_lost, u_det_lost = self._lost_association(
+            activated_stracks,
+            refind_stracks,
+            remaining_dets,
+            active_tracks,
+            unconfirmed,
+            img,
+            dets,
+        )
+        remaining_dets = [remaining_dets[i] for i in u_det_lost]
         
         # final_unmatched_dets, unmatched_lost_tracks_indices = self._lost_association(
         #     activated_stracks,
