17a8e0ff325cc72f09328c2e58c1a3fe016685b7
alaxahml <45281963+alaxahml@users.noreply.github.com>
2025-07-28 16:03:41 +0300

    Update botsort.py

--- boxmot/trackers/botsort/botsort.py
+++ boxmot/trackers/botsort/botsort.py
@@ -217,14 +217,14 @@ class BotSort(BaseTracker):
         # STrack.multi_gmc(unconfirmed, warp)
         # STrack.multi_gmc(self.lost_stracks, warp)
         
-        # STrack.multi_predict(active_tracks)
-        # #STrack.multi_predict(unconfirmed)
-        # STrack.multi_predict(self.lost_stracks)
+        STrack.multi_predict(active_tracks)
+        #STrack.multi_predict(unconfirmed)
+        STrack.multi_predict(self.lost_stracks)
     
-        # warp = self.cmc.apply(img, dets)
-        # STrack.multi_gmc(active_tracks, warp)
-        # STrack.multi_gmc(unconfirmed, warp)
-        # STrack.multi_gmc(self.lost_stracks, warp)
+        warp = self.cmc.apply(img, dets)
+        STrack.multi_gmc(active_tracks, warp)
+        STrack.multi_gmc(unconfirmed, warp)
+        STrack.multi_gmc(self.lost_stracks, warp)
 
         # # --- STAGE 1: Associate Active Tracks (Motion + Appearance) ---
         # STrack.multi_predict(active_tracks)
@@ -248,14 +248,14 @@ class BotSort(BaseTracker):
  
 
         # Fix camera motion
-        STrack.multi_predict(active_tracks)
-        #STrack.multi_predict(unconfirmed)
-        STrack.multi_predict(self.lost_stracks)
+        # STrack.multi_predict(active_tracks)
+        # #STrack.multi_predict(unconfirmed)
+        # STrack.multi_predict(self.lost_stracks)
     
-        warp = self.cmc.apply(img, dets)
-        STrack.multi_gmc(active_tracks, warp)
-        STrack.multi_gmc(unconfirmed, warp)
-        STrack.multi_gmc(self.lost_stracks, warp)
+        # warp = self.cmc.apply(img, dets)
+        # STrack.multi_gmc(active_tracks, warp)
+        # STrack.multi_gmc(unconfirmed, warp)
+        # STrack.multi_gmc(self.lost_stracks, warp)
 
 
 
